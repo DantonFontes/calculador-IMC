4be1ad4d7843c5c810f3a4b66cf8bcc1e9b509ad
DantonFontes <dantonleite2005@gmail.com>
2024-11-27 18:09:34 -0300

    Atualizando o código do projeto

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('./database'); // Importa o arquivo database.js
 const cors = require('cors');
 

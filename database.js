d18eb1beb12eec5b5eca44218d0966beb661a8c3
DantonFontes <dantonleite2005@gmail.com>
2024-11-27 14:35:55 -0300

    Atualização do código

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 // Obtenha o caminho do banco de dados a partir da variável de ambiente
-const dbPath = process.env.DATABASE_PATH || path.resolve(__dirname, 'database.db'); // Caminho padrão caso não haja a variável
+const dbPath = process.env.DB_PATH || path.resolve(__dirname, 'database.db'); // Caminho padrão caso não haja a variável
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados:', err.message);
